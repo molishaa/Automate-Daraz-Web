0bc7ddf1eb5eea6d361300ad1a347e9d276f0469
Molisaha <molisharajbhandari@gmail.com>
2021-04-19 18:51:25 +0545

    filter Checkbox

--- cypress/integration/Daraz.js
+++ cypress/integration/Daraz.js
@@ -13,12 +13,19 @@ cy.get('.mod-input.mod-login-input-loginName.mod-input-loginName').type('molisha
 cy.get('.mod-input.mod-input-password.mod-login-input-password.mod-input-password').type('Test@123')
  cy.get('next-btn next-btn-primary next-btn-large').click()    //login
 });
-it('Search', () => {
+it('Search item', () => {
 cy.get('.search-box__input--O34g').type('Mobile')
 
  });
-//  it('Apply multiple filter and add item to the cart', () => {
+ it('Apply multiple filter and add item to the cart', () => {
+    //checkbox
+     cy.get('input[value=.ant-checkbox-input]').first().click()
+     cy.get('input[value=.ant-checkbox-input]').first().should('be.checked')
+    //  cy.get('Nokia').click()
+    //  cy.get('OnePlus').click()
 
+
+});
 
 });
 
